ef9054aeadc1ad16b4637bea668a1e134ca69c37
JaneMoody <janeemoody2@gmail.com>
2020-12-17 11:45:19 -0500

    getting the hang of this still gotta see if the tests pass though

--- cypress/integration/examples/form_Test.js
+++ cypress/integration/examples/form_Test.js
@@ -1,16 +1,22 @@
 describe("name text", function () {
   it("Checks if text contains name", function () {
-    cy.visit("form.js");
-    cy.get(".name").should("have.value", "Enter Name");
-    cy.type("Jane");
+    cy.get('input[type="name"]').type("Jane").should("have.value", "Jane");
+    // cy.visit("form.js");
+    // cy.get(".name").should("have.value", "Enter Name");
+    // cy.type("Jane");
   });
 });
 describe("email text", function () {
-  it("Checks if text contains email", function () {
-    cy.visit("form.js");
-    cy.get(".Email").should("have.value", "Enter Email");
-    cy.type("moodyjane@gmail.com");
+  it("can type an email address into an input field", () => {
+    cy.get('input[type="email"]')
+      .type("moody@gmail.com")
+      .should("have.value", "moody@gmail.com");
   });
+  //   it("Checks if text contains email", function () {
+  //     cy.visit("form.js");
+  //     cy.get(".Email").should("have.value", "Enter Email");
+  //     cy.type("moodyjane@gmail.com");
+  //   });
 });
 describe("password text", function () {
   it("Checks if text contains password", function () {
